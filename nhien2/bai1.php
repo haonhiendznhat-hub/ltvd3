34f911572f5257b8574f69b649a7ce6df0223a82
haonhiendznhat-hub <haonhiendznhat@gmail.com>
2026-03-05 09:20:56 +0700

    Update bai1.php

--- nhien2/bai1.php
+++ nhien2/bai1.php
@@ -1,10 +1,9 @@
 <?php
-function xapXep(&$mang) { // Dùng dấu & để thay đổi trực tiếp mảng gốc
+function xapXep(&$mang) { 
     $n = count($mang);
     for ($i = 0; $i < $n - 1; $i++) {
         for ($j = 0; $j < $n - $i - 1; $j++) {
             if ($mang[$j] > $mang[$j + 1]) {
-                // Hoán vị
                 $temp = $mang[$j];
                 $mang[$j] = $mang[$j + 1];
                 $mang[$j + 1] = $temp;
@@ -16,3 +15,4 @@ $numbers = [4, 2, 8, 1];
 xapXep($numbers);
 print_r($numbers);
 ?>
+
